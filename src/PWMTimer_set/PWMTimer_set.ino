dd96a1b3d693300123e74b936590ad8ec21643b5
Bigyuun <bigyun@gmail.com>
2022-07-20 17:07:21 +0900

    DC motor encoder count implemetation with interrupt & 20kHz PWM set up done

---
+++ src/PWMTimer_set/PWMTimer_set.ino
@@ -0,0 +1,57 @@
+#define Baudrate 115200
+
+#define POTENTIOMETER A0
+
+#define ENCODER_PHASE_A 7
+#define ENCODER_PAHSE_B 8
+#define MOTOR_PWM_PIN 9 // recommend pin #10 just use for PWM timer
+#define MOTOR_DIRECTION 11
+#define MOTOR_STSP 12
+#define MOTOR_FREQUENCY 799
+
+static int motor_duty = 399; // 50%
+static int enc_counter = 0;
+
+void FastPWMRegisterSet();
+void Initialize();
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Initialize();
+  FastPWMRegisterSet();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //OCR1A = analogRead(POTENTIOMETER); // 0~1023
+
+  Serial.print("Duty : "); Serial.println(OCR1A);
+  Serial.print("Encoder Counter : "); Serial.println(enc_counter);
+  //delay(10);/}
+
+void Initialize()
+{
+  Serial.begin(Baudrate);
+  pinMode(ENCODER_PHASE_A, INPUT);
+  pinMode(ENCODER_PAHSE_B, INPUT);
+  pinMode(MOTOR_PWM_PIN, OUTPUT);
+  pinMode(MOTOR_DIRECTION, OUTPUT);
+  pinMode(MOTOR_STSP, OUTPUT);
+
+  digitalWrite(MOTOR_DIRECTION, HIGH);
+  digitalWrite(MOTOR_STSP, HIGH);
+  
+}
+
+void FastPWMRegisterSet()
+{
+  TCCR1A = bit(COM1A1) | bit(COM1A0) | bit(COM1B1) | bit(COM1B0); //inverting mode => 0일때 HIGH, MAX일때 LOW
+  TCCR1A |= bit(WGM11);
+  TCCR1B = bit(WGM12) | bit(WGM13); // Fast PWM mode using ICR1 as TOP
+  TCCR1B |= bit(CS10); // no prescaler
+  ICR1 = MOTOR_FREQUENCY; //주파수 설정 => 16MHz / ICR1 = 주파수
+  OCR1A = 399; // 듀티 설정 => OCR1A/ICR1
+  OCR1B = 0;
+  TCNT1 = 0;
+}
---
+++ src/interrupt_setup/interrupt_setup.ino
@@ -0,0 +1,141 @@
+#define Baudrate 115200
+
+#define POTENTIOMETER A0
+
+#define ENCODER_PHASE_A 2 // pin #2,3 are only things for using interrupt
+#define ENCODER_PAHSE_B 3
+#define MOTOR_PWM_PIN 9 // recommend pin #10 just use for PWM timer
+#define MOTOR_DIRECTION 11
+#define MOTOR_STSP 12
+#define MOTOR_FREQUENCY 799
+
+void FastPWMRegisterSet();
+void Initialize();
+void EncoderInit();
+void isrA();
+void isrB();
+
+static long g_enc_pos = 0;
+static int motor_duty = 399; // 50%
+static int flag = 0;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Initialize();
+  EncoderInit();
+  FastPWMRegisterSet();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if(flag == 0){
+    Serial.print("Duty : "); Serial.println(OCR1A);
+    Serial.print("Encoder Counter : ");
+    Serial.println(g_enc_pos);  
+    delay(1000);
+  }
+  
+  if(g_enc_pos>=106400)
+  {
+    OCR1A =799;
+    flag = 1;
+  }
+}
+
+void Initialize()
+{
+  
+  Serial.begin(Baudrate);
+
+  pinMode(MOTOR_PWM_PIN, OUTPUT);
+  pinMode(MOTOR_DIRECTION, OUTPUT);
+  pinMode(MOTOR_STSP, OUTPUT);
+
+  digitalWrite(MOTOR_DIRECTION, HIGH);
+  digitalWrite(MOTOR_STSP, HIGH);
+  
+}
+
+void EncoderInit()
+{
+  pinMode(ENCODER_PHASE_A, INPUT);
+  pinMode(ENCODER_PAHSE_B, INPUT);
+  attachInterrupt(0, isrA, CHANGE);
+  attachInterrupt(1, isrB, CHANGE);
+}
+
+void FastPWMRegisterSet()
+{
+  TCCR1A = bit(COM1A1) | bit(COM1A0) | bit(COM1B1) | bit(COM1B0); //inverting mode => 0일때 HIGH, MAX일때 LOW
+  TCCR1A |= bit(WGM11);
+  TCCR1B = bit(WGM12) | bit(WGM13); // Fast PWM mode using ICR1 as TOP
+  TCCR1B |= bit(CS10); // no prescaler
+  ICR1 = MOTOR_FREQUENCY; //주파수 설정 => 16MHz / ICR1 = 주파수
+  OCR1A = 200; // 듀티 설정 => OCR1A/ICR1
+  OCR1B = 0;
+  TCNT1 = 0;
+}
+
+
+void isrA()
+{
+  if (digitalRead(ENCODER_PHASE_A) == HIGH) { 
+    // check channel B to see which way encoder is turning
+    if (digitalRead(ENCODER_PAHSE_B) == LOW) {  
+      g_enc_pos += 1;
+    } 
+    else {
+      g_enc_pos -= 1;
+    }
+  }
+  else   // must be a high-to-low edge on channel A                                       
+  { 
+    // check channel B to see which way encoder is turning  
+    if (digitalRead(ENCODER_PAHSE_B) == HIGH) {   
+      g_enc_pos += 1;
+    } 
+    else {
+      g_enc_pos -= 1;
+    }
+  }
+
+  if(g_enc_pos>=106400)
+  {
+    OCR1A =799;
+    flag = 1;
+  }
+}
+
+void isrB()
+{
+  // look for a low-to-high on channel B
+  if (digitalRead(ENCODER_PAHSE_B) == HIGH) {   
+   // check channel A to see which way encoder is turning
+    if (digitalRead(ENCODER_PHASE_A) == HIGH) {  
+      g_enc_pos += 1;
+    } 
+    else {
+      g_enc_pos -= 1;
+    }
+  }
+  // Look for a high-to-low on channel B
+  else { 
+    // check channel B to see which way encoder is turning  
+    if (digitalRead(ENCODER_PHASE_A) == LOW) {   
+      g_enc_pos += 1;
+    } 
+    else {
+      g_enc_pos -= 1;
+    }
+  }
+  if(g_enc_pos>=106400)
+  {
+    OCR1A =799;
+    flag = 1;
+  }
+  
+}
